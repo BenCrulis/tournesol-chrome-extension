ce28a9b249dcc2f7b6e6b3a55a77cc175e7bf816
Louis Faucon <lpfaucon@gmail.com>
2021-05-08 19:52:27 +0200

    Enable building extension for both Chrome and Firefox
    Note: fix POST request issues with Chrome

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+echo "Building extension for Chrome. . ."
+
+rm tournesol_extension_chrome.zip
+cp manifest_chrome.json manifest.json
+zip -r -FS tournesol_extension_chrome.zip * --exclude '.git'
+rm manifest.json
+
+echo "Building extension for Firefox. . ."
+
+rm tournesol_extension_firefox.zip
+cp manifest_chrome.json manifest.json
+zip -r -FS tournesol_extension_firefox.zip * --exclude '.git'
+rm manifest.json
+
+echo "Done"
