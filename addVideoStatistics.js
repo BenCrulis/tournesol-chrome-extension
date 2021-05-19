a5a63ed288decef038dc52eee44d1d10040559c0
ben <bencrulis@gmail.com>
2021-05-20 00:07:07 +0200

    Added statistics panel with currently only a plotly bar chart of the criterias

--- addVideoStatistics.js
+++ addVideoStatistics.js
@@ -5,6 +5,77 @@
 
 var browser = browser || chrome;
 
+const criterias = [
+	"reliability",
+	"importance",
+	"engaging",
+	"pedagogy",
+	"layman_friendly",
+	"diversity_inclusion",
+	"backfire_risk",
+	"better_habits",
+	"entertaining_relaxing"
+];
+
+
+const CRITERIAS = [
+	{
+		key: "reliability",
+		name: "reliable and not misleading",
+		color: "#6573C3",
+		description: "no description yet"
+	},
+	{
+		key: "importance",
+		name: "important and actionable",
+		color: "#FF784E",
+		description: "no description yet"
+	},
+	{
+		key: "engaging",
+		name: "engaging and though-provoking",
+		color: "#FFC107",
+		description: "no description yet"
+	},
+	{
+		key: "pedagogy",
+		name: "clear and pedagogical",
+		color: "#8561C5",
+		description: "no description yet"
+	},
+	{
+		key: "layman_friendly",
+		name: "layman-friendly",
+		color: "#50F22C",
+		description: "no description yet"
+	},
+	{
+		key: "diversity_inclusion",
+		name: "diversity and inclusion",
+		color: "#2CEFF2",
+		description: "no description yet"
+	},
+	{
+		key: "backfire_risk",
+		name: "resilience to backfiring risks",
+		color: "#E91E63",
+		description: "no description yet"
+	},
+	{
+		key: "better_habits",
+		name: "encourages better habits",
+		color: "#C6EB34",
+		description: "no description yet"
+	},
+	{
+		key: "entertaining_relaxing",
+		name: "entertaining and relaxing",
+		color: "#6BA4FF",
+		description: "no description yet"
+	},
+];
+
+
 document.addEventListener('yt-navigate-finish', process);
 
 if (document.body) process();
@@ -74,10 +145,75 @@ function process() {
         statisticsTextTd.append(statisticsTextTdText);
         statisticsButton.append(statisticsTextTd);
 
+		// statistics panel
+		statsPanel = document.createElement("div");
+		statsPanel.setAttribute('id', "tournesol-stats-panel");
+		statsPanel.style.display = 'none';
+
+		const PANEL_DISPLAY = 'block';
+
+		function toggle_panel() {
+			if (statsPanel.style.display === "none") {
+				statsPanel.style.display = PANEL_DISPLAY;
+			}
+			else {
+				statsPanel.style.display = 'none';
+			}
+		}
+
+		plotlyBarChart = document.createElement("div");
+
+		console.log(details);
+
+		criteria_values = CRITERIAS.map((c) => details[c["key"]]).reverse();
+		criteria_names = CRITERIAS.map((c) => c["name"]).reverse();
+
+		console.log(criteria_values);
+
+		var data = [{
+			type: 'bar',
+			x: criteria_values,
+			y: criteria_names,
+			marker: {
+				color: CRITERIAS.map((c) => c["color"]).reverse(),
+			},
+			orientation: 'h',
+			width: 0.5
+		}];
+
+		const layout = {
+			width: 720,
+			height: 250,
+			title: "criteria scores",
+			//font: {size: 18},
+			dragmode: false, // prevent resizing by clic and drag
+
+			margin: {
+			    l: 230,
+			    r: 20,
+			    t: 30,
+			    b: 20
+		    },
+
+		};
+
+		const config = {
+			displayModeBar: false,
+		};
+
+        Plotly.newPlot(plotlyBarChart, data, layout, config);
+
+		statsPanel.append(plotlyBarChart);
+
+		detailsLink = document.createElement("a")
+		detailsLink.setAttribute('href', `https://tournesol.app/details/${videoId}`)
+		detailsLink.innerText = "see more details on tournesol.app"
+		statsPanel.append(detailsLink);
+
+		statisticsButton.append(statsPanel);
+
         // On click
-        statisticsButton.onclick = () => {
-          open(`https://tournesol.app/details/${videoId}`)
-        }
+        statisticsButton.onclick = toggle_panel;
 
         var div = document
           .getElementById('menu-container')
