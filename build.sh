70d363eb080628d42939d67491d006f3702d7f43
Aidan <53896963+aidanjungo@users.noreply.github.com>
2021-05-24 14:04:55 +0200

    Exclude zip files when builing the new zip files (#34)

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ echo "Building extension for Chrome. . ."
 
 rm tournesol_extension_chrome.zip
 cp manifest_chrome.json manifest.json
-zip -r -FS tournesol_extension_chrome.zip * --exclude '.git'
+zip -r -FS tournesol_extension_chrome.zip * -x *.git* *.zip*
 rm manifest.json
 
 echo "Building extension for Firefox. . ."
 
 rm tournesol_extension_firefox.zip
 cp manifest_firefox.json manifest.json
-zip -r -FS tournesol_extension_firefox.zip * --exclude '.git'
+zip -r -FS tournesol_extension_firefox.zip * -x *.git* *.zip*
 rm manifest.json
 
 echo "Done"
